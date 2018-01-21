810fb080168ad1240c87422d5d571b871b7ca7d8
AndrewJenson <awjenson@gmail.com>
2018-01-20 16:41:39 -0800

    Firebase get complete

--- SocialMediaApp/Controller/FeedVC.swift
+++ SocialMediaApp/Controller/FeedVC.swift
@@ -14,6 +14,9 @@ class FeedVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var tableView: UITableView!
 
+    // Initialize Array of Post objects
+    var posts = [Post]()
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,9 +25,21 @@ class FeedVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
         // Reference the database
         DataService.ds.REF_POSTS.observe(.value) { (snapshot) in
-            print(snapshot.value)
+            // Breakout into individual objects
+            if let snapshot = snapshot.children.allObjects as? [DataSnapshot] {
+                // get each individual snapshot
+                for snap in snapshot {
+                    print("SNAP: \(snap)")
+                    if let postDict = snap.value as? Dictionary<String, AnyObject> {
+                        let key = snap.key
+                        let post = Post(postKey: key, postData: postDict)
+                        // add to post property
+                        self.posts.append(post)
+                    }
+                }
+            }
+            self.tableView.reloadData()
         }
-
     }
 
     // Table View
@@ -33,10 +48,15 @@ class FeedVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+
+        return posts.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        let post = posts[indexPath.row]
+        print("ANDREW: \(post.caption)")
+
         return tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
     }
 
